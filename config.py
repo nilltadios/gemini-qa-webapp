ce039faedf33210bb4c6eed2c6d2bf16f343f362
nilltadios <name49843@gmail.com>
2025-10-19 00:32:54 -0400

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,32 @@ DEFAULT_MAX_REFINEMENTS = 3
 WORD_COUNT_TOLERANCE = 0.10  # 10% tolerance
 
 # File Settings
-SUPPORTED_EXTENSIONS = ['txt', 'pdf', 'md', 'py', 'json', 'csv', 'm']
+SUPPORTED_EXTENSIONS = [
+    # Images
+    "png", "jpg", "jpeg", "webp", "heic", "heif",
+    
+    # Documents
+    "pdf", "txt", "md", "rtf",
+    
+    # Code files
+    "py", "js", "java", "c", "cpp", "cs", "go", "rb", "php", "swift", "kt",
+    "m", "r", "sql", "sh", "bash", "yml", "yaml", "json", "xml", "html", "css",
+    
+    # Data files
+    "csv", "tsv", "xls", "xlsx",
+    
+    # Audio
+    "mp3", "wav", "mpeg", "m4a", "ogg", "flac",
+    
+    # Video
+    "mp4", "mov", "avi", "wmv", "flv", "mpg", "mpeg", "webm", "mkv",
+    
+    # Archives (if supported by your application)
+    "zip", "tar", "gz",
+    
+    # Other
+    "log", "cfg", "conf", "ini"
+]
 TEXT_ENCODINGS = ['utf-8', 'latin-1', 'cp1252']
 
 # UI Settings
